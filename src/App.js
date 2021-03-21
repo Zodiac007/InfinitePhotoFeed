85e55d460dc4866998f5ed91d571d1bbebc2d57a
Harsh Dwivedi <harsh007dwivedi@gmail.com>
2021-03-22 05:09:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loader from "./Loader";
+import { SRLWrapper } from "simple-react-lightbox";
+import "./App.css";
 
-function App() {
+const accessKey = "VMUOeKDkdlgK8gK_i2mHCJ5bf0cijIgD08Nt1UhtY8M";
+
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+
+  const getPhotos = useCallback(() => {
+    let apiUrl = `https://api.unsplash.com/photos?`;
+    if (query) apiUrl = `https://api.unsplash.com/search/photos?query=${query}`;
+
+    apiUrl += `&page=${page}`;
+    apiUrl += `&client_id=${accessKey}`;
+
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        const imagesFromApi = data.results ?? data;
+
+        if (page === 1) setImages(imagesFromApi);
+
+        setImages((images) => [...images, ...imagesFromApi]);
+      });
+  }, [page, query]);
+
+  useEffect(() => {
+    getPhotos();
+  }, [page, getPhotos]);
+
+  function searchPhotos(e) {
+    e.preventDefault();
+    setPage(1);
+    getPhotos();
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Infinite Photo Feed</h1>
+
+      <form onSubmit={searchPhotos}>
+        <input
+          type="text"
+          placeholder="Search Photos.."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => setPage((page) => page + 1)}
+        hasMore={true}
+        loader={<Loader />}
+      >
+        <SRLWrapper>
+          <div className="image-grid">
+            {images.map((image, index) => (
+              <a className="image" key={index} href={image.urls.thumb}>
+                <img src={image.urls.regular} alt="" />
+              </a>
+            ))}
+          </div>
+        </SRLWrapper>
+      </InfiniteScroll>
     </div>
   );
 }
-
-export default App;
